bf8f25ec886943ef786c745151c2a6ddb8c508de
CanalDeMemes123 <103776979+CanalDeMemes123@users.noreply.github.com>
2022-07-30 16:37:05 -0300

    Update SongName__Port.lua

--- assets/preload/data/prey/SongName__Port.lua
+++ assets/preload/data/prey/SongName__Port.lua
@@ -1,5 +1,5 @@
 function onCreate()
-makeLuaText('watermark', songName .. " | Ported By Bruh", 0, 25, 600); -- You can edit the created by Prisma Text just dont change anything else
+makeLuaText('watermark', songName .. " | Ported By Canal De Memes", 0, 25, 600); -- You can edit the created by Prisma Text just dont change anything else
     setTextSize('watermark', 20);
     addLuaText('watermark');
 end
